c07c920d8f75a09049c899333b36eb8ac5ae658b
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-01 18:34:33 -0500

    Initial commit

---
+++ crm/src/main/java/com/crm/crm/model/Contact.java
@@ -0,0 +1,26 @@
+package com.crm.crm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Contact {
+
+    private @Id @GeneratedValue long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private Contact(){}
+
+    public Contact(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+}
---
+++ crm/src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,14 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+
+public class ReactAndSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+
+}
